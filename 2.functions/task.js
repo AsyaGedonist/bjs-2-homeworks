5e261873dd38f2524204387689ce156d2424e671
Anastasiia Tamonova <ice_asa@mail.ru>
2022-11-01 13:37:06 +0300

    2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,57 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i<arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  let avg = Number((sum/arr.length).toFixed(2));
 
-  return { min: min, max: max, avg: avg };
+  return (`min: ${min}, max: ${max}, avg: ${avg}`);
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let a of arr){
+    sum += a;
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = 0;
+  for (let arr of arrOfArr) {
+    let sum = func(arr);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let difference = 0;
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 0; i<arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  difference = Math.abs(max - min);
+  return difference;
 }
