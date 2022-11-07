49ccfdd87f2fd4fbd0ec6da44bf06f7d2d19f114
Anastasiia Tamonova <ice_asa@mail.ru>
2022-11-07 16:41:01 +0300

    4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,33 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark){  
+  if (parseInt(mark) >=1 && parseInt(mark) <= 5 ){
+    if (this.marks === undefined){
+      this.marks = [parseInt(mark)];
+    } else {
+      this.marks.push(parseInt(mark));
+    }
+  } else {
+    console.log("Введена некорректная оценка");
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  this.marks.forEach(item => (sum += item));
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
